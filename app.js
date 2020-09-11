7e1a7529f9465b3ea49f345910457110c495c4d7
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-11 17:09:18 +0000

    Node App Added

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to Codecomp3 Node Player').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
