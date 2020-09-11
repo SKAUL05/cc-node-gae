a066ff5d6248eba3ebe755bd04e631e7523fbb8e
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-11 17:17:40 +0000

    App JS Changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to Codecomp3 Node Player').end();
+  res.status(200).send('Welcome to CodeComp3 Node Player').end();
 });
 
 // Start the server
